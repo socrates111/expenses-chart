d7417f4b26ae2ea8d3170f42979259a3a8ce7f87
socrates111 <96205696+socrates111@users.noreply.github.com>
2024-04-24 21:13:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const monday = document.querySelector('.a1')
+const wednsday = document.querySelector('.a3');
+const thursday = document.querySelector('.a4');
+const info = document.querySelector('.info');
+
+monday.addEventListener('click', function (){
+    info.classList.toggle('m');
+});
+
+const mond = function() {
+    
+}
+
+
+
+[
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
